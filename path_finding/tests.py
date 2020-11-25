675dd9eadee629ad98c48a864ea51cf79138e84a
Eric Dang <edang830@gmail.com>
2020-11-24 16:33:28 -0800

    Add template test for path_finding view

--- path_finding/tests.py
+++ path_finding/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+import pytest
+from django.urls import reverse
 
-# Create your tests here.
+
+@pytest.mark.unit
+class TestViews:
+
+    def test_path_finding_view_renders_instruction_template(self, client):
+        url = reverse('path-finding')
+
+        response = client.get(url)
+
+        assert response.status_code == 200
+        assert 'path_finding.html' in (template.name for template in response.templates)
