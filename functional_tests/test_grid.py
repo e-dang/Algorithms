676959dc3cfdc0cb9d0273c4df253d74c1c8df4e
Eric Dang <edang830@gmail.com>
2020-11-24 21:53:29 -0800

    Pass default grid params to template

--- functional_tests/test_grid.py
+++ functional_tests/test_grid.py
@@ -5,6 +5,8 @@ from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.action_chains import ActionChains
 
+from path_finding.views import DEFAULT_GRID_PARAMS as grid_params
+
 MAX_WAIT = 10
 
 
@@ -22,19 +24,21 @@ def wait(fn):
     return modified_fn
 
 
+@pytest.fixture(params=['firefox'], scope="class")
+def driver_init(request):
+    request.cls.browser = webdriver.Firefox()
+
+    yield
+
+    request.cls.browser.quit()
+
+
 @pytest.mark.functional
+@pytest.mark.usefixtures("driver_init")
 class TestGrid:
     @pytest.fixture(autouse=True)
     def url(self):
-        return 'localhost:8000/path-finding/'
-
-    @pytest.fixture(autouse=True)
-    def browser(self):
-        browser = webdriver.Firefox()
-
-        yield browser
-
-        browser.quit()
+        return 'http://localhost:8000/path-finding/'
 
     @wait
     def wait_for_row_in_grid_info_table(self, row_text, browser):
@@ -53,30 +57,33 @@ class TestGrid:
     def make_node_id(self, row, col, num_cols):
         return f'n{row * num_cols + col}'
 
-    def test_user_can_customize_grid(self, browser, url):
-        # The user goes to the path finding algorithm's website
-        browser.get(url)
+    def test_user_can_customize_grid(self, url):
+        # The user goes to the Path Finding Algorithm website
+        self.browser.get(url)
 
         # The user notices the page title and header mention path finding algorithms
-        assert 'Path Finding Algorithms' in browser.title
-        header_text = browser.find_element_by_tag_name('h1').text
+        assert 'Path Finding Algorithms' in self.browser.title
+        header_text = self.browser.find_element_by_tag_name('h1').text
         assert 'Path Finding Algorithms' in header_text
 
         # A grid is visible on the page along with information about its dimensions and where the start and end
         # nodes are
-        grid = browser.find_element_by_class_name('grid')
-        self.wait_for_row_in_grid_info_table('Dimensions:', browser)
-        self.wait_for_row_in_grid_info_table('Start:', browser)
-        self.wait_for_row_in_grid_info_table('End:', browser)
+        grid = self.browser.find_element_by_class_name('grid')
+        table = self.browser.find_element_by_id('gridInfo')
+        row_text = [row.text for row in table.find_elements_by_tag_name('tr')]
+        assert len(grid.find_elements_by_class_name('node')) == grid_params['num_rows'] * grid_params['num_cols']
+        assert 'Dimensions {} {}'.format(grid_params['num_rows'], grid_params['num_cols']) in row_text
+        assert 'Start Node {} {}'.format(grid_params['start_row'], grid_params['start_col']) in row_text
+        assert 'End Node {} {}'.format(grid_params['end_row'], grid_params['end_col']) in row_text
 
         # The user also notices a form that enables the grid dimensions, start, and end nodes to be customized
-        row_input = browser.find_element_by_id('rowInput')
-        col_input = browser.find_element_by_id('colInput')
-        start_row_input = browser.find_element_by_id('startRowInput')
-        start_col_input = browser.find_element_by_id('startColInput')
-        end_row_input = browser.find_element_by_id('endRowInput')
-        end_col_input = browser.find_element_by_id('endColInput')
-        submit_button = browser.find_element_by_id('submitButton')
+        row_input = self.browser.find_element_by_id('rowInput')
+        col_input = self.browser.find_element_by_id('colInput')
+        start_row_input = self.browser.find_element_by_id('startRowInput')
+        start_col_input = self.browser.find_element_by_id('startColInput')
+        end_row_input = self.browser.find_element_by_id('endRowInput')
+        end_col_input = self.browser.find_element_by_id('endColInput')
+        submit_button = self.browser.find_element_by_id('submitButton')
 
         # The user enters a new grid size, start, and end points and submits the form. A new grid appears with the
         # correct dimensions, start, and end points
@@ -89,43 +96,43 @@ class TestGrid:
         start_col_input.send_keys(str(start_col))
         end_row_input.send_keys(str(end_row))
         end_col_input.send_keys(str(end_col))
-        ActionChains(browser).move_to_element(submit_button).click().perform()
+        ActionChains(self.browser).move_to_element(submit_button).click().perform()
 
         self.wait_for_assert(
-            lambda: len(browser.find_element_by_class_name('.node')) == num_rows * num_cols
+            lambda: len(self.browser.find_element_by_class_name('.node')) == num_rows * num_cols
         )
         self.wait_for_assert(
-            lambda: browser.find_element_by_class_name('.startNode').id == self.make_node_id(
+            lambda: self.browser.find_element_by_class_name('.startNode').id == self.make_node_id(
                 start_row, start_col, num_cols
             )
         )
         self.wait_for_assert(
-            lambda: browser.find_element_by_class_name('.endNode').id == self.make_node_id(
+            lambda: self.browser.find_element_by_class_name('.endNode').id == self.make_node_id(
                 end_row, end_col, num_cols
             )
         )
 
         # The user then clicks on an empty box in the grid and immediately sees it turn black.
-        element = browser.get_element_by_id(self.make_node_id(5, 5, num_cols))
-        ActionChains(browser).move_to_element(element).click().perform()
+        element = self.browser.get_element_by_id(self.make_node_id(5, 5, num_cols))
+        ActionChains(self.browser).move_to_element(element).click().perform()
         self.wait_for_assert(lambda: element.props.style.backgroundColor == 'rgb(0,0,0)')
 
         # The user then clicks and holds down on an empty box in the grid and drags their mouse across multiple
         # boxes which causes each box to turn black
-        elements = [browser.get_element_by_id(self.make_node_id(i, 4, num_cols)) for i in range(10)]
-        ActionChains(browser).move_to_element(elements[0]).click_and_hold(
+        elements = [self.browser.get_element_by_id(self.make_node_id(i, 4, num_cols)) for i in range(10)]
+        ActionChains(self.browser).move_to_element(elements[0]).click_and_hold(
         ).move_to_element(elements[-1]).release().perform()
         for element in elements:
             self.wait_for_assert(lambda: element.props.style.backgroundColor == 'rgb(0,0,0)')
 
         # The user then clicks on a black box and sees it go back to white
-        element = browser.get_element_by_id(self.make_node_id(5, 5, num_cols))
-        ActionChains(browser).move_to_element(element).click().perform()
+        element = self.browser.get_element_by_id(self.make_node_id(5, 5, num_cols))
+        ActionChains(self.browser).move_to_element(element).click().perform()
         self.wait_for_assert(lambda: element.props.style.backgroundColor == 'rgb(255,255,255)')
 
         # The user clicks and holds down on a black box and drags their mouse across more black boxes which then turn
         # back to white
-        ActionChains(browser).move_to_element(elements[0]).click_and_hold(
+        ActionChains(self.browser).move_to_element(elements[0]).click_and_hold(
         ).move_to_element(elements[-1]).release().perform()
         for element in elements:
             self.wait_for_assert(lambda: element.props.style.backgroundColor == 'rgb(255,255,255)')
--- path_finding/views.py
+++ path_finding/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+DEFAULT_GRID_PARAMS = {
+    'num_rows': 50,
+    'num_cols': 50,
+    'start_row': 9,
+    'start_col': 9,
+    'end_row': 40,
+    'end_col': 40
+}
 
 
 def path_finding(request):
-    return render(request, 'path_finding.html')
+    return render(request, 'path_finding.html', context=DEFAULT_GRID_PARAMS)
