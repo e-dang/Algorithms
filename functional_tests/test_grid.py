e1e263b0c256ac773161937e7b91082ef31ac2f5
Eric Dang <edang830@gmail.com>
2020-11-26 21:34:24 -0800

    Refactor functional test using page pattern

--- functional_tests/test_grid.py
+++ functional_tests/test_grid.py
@@ -1,36 +1,17 @@
-import time
-
 import pytest
-from selenium import webdriver
-from selenium.common.exceptions import WebDriverException
-from selenium.webdriver.common.action_chains import ActionChains
-
 from path_finding.views import DEFAULT_GRID_PARAMS as grid_params
+from selenium import webdriver
 
-MAX_WAIT = 10
-
-
-def wait(fn):
-    def modified_fn(*args, **kwargs):
-        start_time = time.time()
-        while True:
-            try:
-                return fn(*args, **kwargs)
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(0.5)
-
-    return modified_fn
+from .pages.grid_page import GridPage
 
 
 @pytest.fixture(params=['firefox'], scope="class")
 def driver_init(request):
-    request.cls.browser = webdriver.Firefox()
+    request.cls.driver = webdriver.Firefox()
 
     yield
 
-    request.cls.browser.quit()
+    request.cls.driver.quit()
 
 
 @pytest.mark.functional
@@ -40,99 +21,62 @@ class TestGrid:
     def url(self):
         return 'http://localhost:8000/path-finding/'
 
-    @wait
-    def wait_for_row_in_grid_info_table(self, row_text, browser):
-        table = browser.find_element_by_id('gridInfo')
-        rows = table.find_elements_by_tag_name('tr')
-        assert row_text in [row.text for row in rows]
-
-    @wait
-    def wait_for(self, fn):
-        return fn()
-
-    @wait
-    def wait_for_assert(self, fn):
-        assert fn()
-
-    def make_node_id(self, row, col, num_cols):
-        return f'n{row * num_cols + col}'
-
-    def extract_dimensions(self, element):
-        width = int(element.value_of_css_property('width')[:-2])
-        height = int(element.value_of_css_property('height')[:-2])
-        return width, height
-
-    def test_user_can_customize_grid(self, url):
-        # The user goes to the Path Finding Algorithm website
-        self.browser.get(url)
+    def test_user_can_customize_grid_through_input_fields(self, url):
+        # A user goes to the website
+        self.driver.get(url)
+        page = GridPage(self.driver)
 
         # The user notices the page title and header mention path finding algorithms
-        assert 'Path Finding Algorithms' in self.browser.title
-        header_text = self.browser.find_element_by_tag_name('h1').text
-        assert 'Path Finding Algorithms' in header_text
+        assert page.has_correct_title()
+        assert page.has_correct_header()
 
-        # A grid is visible on the page along with information about its dimensions and where the start and end
-        # nodes are
-        grid = self.browser.find_element_by_id('grid')
-        table = self.browser.find_element_by_id('gridInfo')
-        row_text = [row.text for row in table.find_elements_by_tag_name('tr')]
-        g_width, g_height = self.extract_dimensions(grid)
-        nodes = grid.find_elements_by_class_name('node')
-        n_widths, n_heights = zip(*[self.extract_dimensions(node) for node in nodes])
-        assert g_width == sum(n_widths[:grid_params['num_cols']]) + grid_params['num_cols']
-        assert g_height == sum(n_heights[:grid_params['num_rows']]) + grid_params['num_rows']
-        assert len(grid.find_elements_by_class_name('node')) == grid_params['num_rows'] * grid_params['num_cols']
-        assert 'Dimensions {} {}'.format(grid_params['num_rows'], grid_params['num_cols']) in row_text
-        assert 'Start Node {} {}'.format(grid_params['start_row'], grid_params['start_col']) in row_text
-        assert 'End Node {} {}'.format(grid_params['end_row'], grid_params['end_col']) in row_text
+        # A grid is visible on the page, with start and end nodes
+        assert page.grid_has_dimensions(grid_params['num_rows'], grid_params['num_cols'])
 
-        # The user also notices a form that enables the grid dimensions, start, and end nodes to be customized
-        dims_input = self.browser.find_element_by_id('dimensionsInput')
-        start_node_input = self.browser.find_element_by_id('startNodeInput')
-        end_node_input = self.browser.find_element_by_id('endNodeInput')
-        submit_button = self.browser.find_element_by_id('submitButton')
-        assert 'e.g. 50,50' in dims_input.get_attribute('placeholder')
-        assert 'e.g. 10,10' in start_node_input.get_attribute('placeholder')
-        assert 'e.g. 40,40' in end_node_input.get_attribute('placeholder')
+        # There is also information about the grid's dimensions and locations of the start and end nodes
+        assert page.table_displays_dimensions(grid_params['num_rows'], grid_params['num_cols'])
+        assert page.table_displays_start_node_coords(grid_params['start_row'], grid_params['start_col'])
+        assert page.table_displays_end_node_coords(grid_params['end_row'], grid_params['end_col'])
 
-        # The user enters a new grid size, start, and end points and submits the form. A new grid appears with the
-        # correct dimensions, start, and end points
-        num_rows, num_cols = 10, 10
-        start_row, start_col = 2, 2
-        end_row, end_col = 8, 8
-        dims_input.send_keys(f'{num_rows},{num_cols}')
-        start_node_input.send_keys(f'{start_row},{start_col}')
-        end_node_input.send_keys(f'{end_row},{end_col}')
-        submit_button.click()
+        # The user also notices a form that enables the grid dimensions, start, and end nodes to be customized.
+        # The user enters a new grid size, start, and end points and submits the form.
+        dims, start, end = 10, 2, 7
+        page.dims_input = f'{dims},{dims}'
+        page.start_node_input = f'{start},{start}'
+        page.end_node_input = f'{end},{end}'
+        page.click_submit()
 
-        grid = self.browser.find_element_by_id('grid')
-        assert len(grid.find_elements_by_class_name('node')) == num_rows * num_cols
-        assert grid.find_element_by_class_name('node.start').get_attribute('id') == self.make_node_id(
-            start_row, start_col, num_cols)
-        assert grid.find_element_by_class_name('node.end').get_attribute('id') == self.make_node_id(
-            end_row, end_col, num_cols)
+        # A new grid appears with the correct dimensions, start, and end nodes
+        assert page.grid_has_dimensions(dims, dims)
+        assert page.is_node_of_type(start, start, 'start')
+        assert page.is_node_of_type(end, end, 'end')
 
-        # The user then clicks on an empty box in the grid and immediately sees it turn black.
-        element = grid.find_element_by_id(self.make_node_id(5, 5, num_cols))
-        element.click()
-        assert element.get_attribute('class') == 'node wall'
+    def test_user_can_change_node_types_between_wall_and_empty(self, url):
+        # The user goes to the website and sees a grid
+        self.driver.get(url)
+        page = GridPage(self.driver)
 
-        # The user then clicks and holds down on an empty box in the grid and drags their mouse across multiple
-        # boxes which causes each box to turn black
-        elements = [grid.find_element_by_id(self.make_node_id(i, 4, num_cols)) for i in range(10)]
-        ActionChains(self.browser).move_to_element(elements[0]).click_and_hold(
-        ).move_by_offset(5, 5).move_to_element(elements[-1]).release().perform()
-        for element in elements:
-            assert element.get_attribute('class') == 'node wall'
+        # The user clicks on an empty node in the grid and immediately sees it turn to a wall node.
+        row, col = 5, 5
+        assert page.is_node_of_type(row, col, 'empty')
+        page.click_node(row, col)
+        assert page.is_node_of_type(row, col, 'wall')
 
-        # The user then clicks on a black box and sees it go back to white
-        element = self.browser.find_element_by_id(self.make_node_id(5, 5, num_cols))
-        ActionChains(self.browser).move_to_element(element).click().perform()
-        assert element.get_attribute('class') == 'node empty'
+        # The user clicks on the node again and sees it go back to an empty node
+        page.click_node(row, col)
+        assert page.is_node_of_type(row, col, 'empty')
 
-        # The user clicks and holds down on a black box and drags their mouse across more black boxes which then turn
-        # back to white
-        ActionChains(self.browser).move_to_element(elements[0]).click_and_hold(
-        ).move_by_offset(5, 5).move_to_element(elements[-1]).release().perform()
-        for element in elements:
-            assert element.get_attribute('class') == 'node empty'
+        # The user then clicks and holds down on an empty node in the grid and drags their mouse across multiple
+        # nodes which causes each node to turn black
+        start_row, end_row, col = 0, 10, 4
+        for i in range(start_row, end_row + 1):
+            assert page.is_node_of_type(i, col, 'empty')
+        page.click_and_hold_nodes(start_row, col, end_row, col)
+        for i in range(start_row, end_row + 1):
+            assert page.is_node_of_type(i, col, 'wall')
+
+        # The user clicks and holds down on a wall node and drags their mouse across more wall nodes which then turn
+        # back to empty nodes
+        page.click_and_hold_nodes(start_row, col, end_row, col)
+        for i in range(start_row, end_row + 1):
+            assert page.is_node_of_type(i, col, 'empty')
