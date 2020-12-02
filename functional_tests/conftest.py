1ade5a2233f98f7a06e5a9d9902b11bc3f85b0e8
Eric Dang <edang830@gmail.com>
2020-12-01 23:06:04 -0800

    Add headless option to pytest

--- functional_tests/conftest.py
+++ functional_tests/conftest.py
@@ -1,10 +1,20 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver import FirefoxOptions
 
 
-@pytest.fixture(params=['firefox'], scope="class")
-def driver_init(request):
-    request.cls.driver = webdriver.Firefox()
+def pytest_addoption(parser):
+    parser.addoption('--headless', action='store_true', default='Whether to run the tests headless or not.')
+
+
+@pytest.fixture(params=['firefox'], scope='class')
+def driver_init(request, pytestconfig):
+    if pytestconfig.getoption('headless'):
+        opts = FirefoxOptions()
+        opts.add_argument("--headless")
+        request.cls.driver = webdriver.Firefox(firefox_options=opts)
+    else:
+        request.cls.driver = webdriver.Firefox()
 
     yield
 
