b1b6b40f7dd03cd2fdf29a0248b299415e93dbee
Eric Dang <edang830@gmail.com>
2020-12-06 19:58:30 -0800

    Implement greedy-bfs

--- path_finding/static/src/algorithms/greedy-bfs.js
+++ path_finding/static/src/algorithms/greedy-bfs.js
@@ -1,3 +1,55 @@
-class GreedyBestFirstSearch {}
+const BaseAlgorithm = require('./base_algorithm');
+const NodeMinHeap = require('../utils/node_min_heap');
+
+class GreedyBestFirstSearch extends BaseAlgorithm {
+    async run(callback) {
+        const endNode = this.grid.getEndNode();
+        const startNode = this.grid.getStartNode();
+        const heap = new NodeMinHeap('heuristicScore');
+        startNode.totalCost = 0;
+        startNode.heuristicScore = this.calcHeuristic(startNode, endNode);
+        heap.insert(startNode);
+
+        while (!heap.isEmpty()) {
+            const node = heap.pop();
+            await this.visit(node);
+
+            if (node.isEndNode()) {
+                break;
+            }
+
+            for (let i = 0; i < this.dr.length; i++) {
+                const row = node.row + this.dr[i];
+                const col = node.col + this.dc[i];
+
+                if (this.grid.isInvalidSpace(row, col)) {
+                    continue;
+                }
+
+                const candidateNode = this.grid.getNode(row, col);
+                const cost = node.totalCost + candidateNode.cost;
+                if (candidateNode.totalCost > cost) {
+                    const heuristicScore = this.calcHeuristic(candidateNode, endNode);
+                    if (heap.contains(candidateNode)) {
+                        heap.update(candidateNode, heuristicScore);
+                    } else {
+                        candidateNode.heuristicScore = heuristicScore;
+                        heap.insert(candidateNode);
+                        await this.visiting(candidateNode);
+                    }
+
+                    candidateNode.totalCost = cost;
+                    candidateNode.prev = node;
+                }
+            }
+        }
+
+        callback(endNode.totalCost);
+    }
+
+    calcHeuristic(node1, node2) {
+        return Math.sqrt(Math.pow(node1.row - node2.row, 2) + Math.pow(node1.col - node2.col, 2));
+    }
+}
 
 module.exports = GreedyBestFirstSearch;
