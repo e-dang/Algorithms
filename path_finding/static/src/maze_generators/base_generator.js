9a78a300a3ce60b772bac18705071acaf5ed8691
Eric Dang <edang830@gmail.com>
2020-12-09 18:18:40 -0800

    Move connect and getNeighbors into base_generator

--- path_finding/static/src/maze_generators/base_generator.js
+++ path_finding/static/src/maze_generators/base_generator.js
@@ -1,8 +1,40 @@
+const utils = require('../utils/utils');
 class BaseGenerator {
     constructor(grid) {
         this.grid = grid;
         this.dr = [2, 0, -2, 0];
         this.dc = [0, 2, 0, -2];
+        this.grid.nodes.forEach((node) => node.setAsWallNode());
+    }
+
+    async connect(node1, node2) {
+        await utils.sleep(10);
+
+        node1.setAsEmptyNode();
+        node2.setAsEmptyNode();
+
+        const betweenRow = Math.floor((node1.row + node2.row) / 2);
+        const betweenCol = Math.floor((node1.col + node2.col) / 2);
+        this.grid.getNode(betweenRow, betweenCol).setAsEmptyNode();
+    }
+
+    getNeighbors(node, isOfType) {
+        const neighbors = [];
+        for (let i = 0; i < this.dr.length; i++) {
+            const row = node.row + this.dr[i];
+            const col = node.col + this.dc[i];
+
+            if (row < 0 || row >= this.grid.nRows || col < 0 || col >= this.grid.nCols) {
+                continue;
+            }
+
+            const neighbor = this.grid.getNode(row, col);
+            if (isOfType(neighbor)) {
+                neighbors.push(neighbor);
+            }
+        }
+
+        return neighbors;
     }
 }
 
--- path_finding/static/src/maze_generators/rand_dfs.js
+++ path_finding/static/src/maze_generators/rand_dfs.js
@@ -3,14 +3,14 @@ const utils = require('../utils/utils');
 
 class RandomizedDFS extends BaseGenerator {
     async generate() {
-        this.grid.nodes.forEach((node) => node.setAsWallNode());
         await this.generateHelper(this.grid.getStartNode());
+        this.grid.nodes.forEach((node) => (node.visited = false));
     }
 
     async generateHelper(node) {
         node.visited = true;
         while (true) {
-            const neighbors = this.getUnvisitedNeighbors(node);
+            const neighbors = this.getNeighbors(node, (node) => !node.visited);
 
             if (neighbors.length === 0) {
                 break;
@@ -19,45 +19,6 @@ class RandomizedDFS extends BaseGenerator {
             const neighbor = utils.getRandom(neighbors);
             await this.connect(node, neighbor);
             await this.generateHelper(neighbor);
-        }
-    }
-
-    getUnvisitedNeighbors(node) {
-        const neighbors = [];
-        for (let i = 0; i < this.dr.length; i++) {
-            const row = node.row + this.dr[i];
-            const col = node.col + this.dc[i];
-
-            if (row < 0 || row >= this.grid.nRows || col < 0 || col >= this.grid.nCols) {
-                continue;
-            }
-
-            const neighbor = this.grid.getNode(row, col);
-            if (!neighbor.visited) {
-                neighbors.push(neighbor);
-            }
-        }
-
-        return neighbors;
-    }
-
-    async connect(node1, node2) {
-        await utils.sleep(10);
-
-        node1.setAsEmptyNode();
-        node2.setAsEmptyNode();
-        if (node1.row === node2.row) {
-            if (node1.col - node2.col < 0) {
-                this.grid.getNode(node1.row, node1.col + 1).setAsEmptyNode();
-            } else {
-                this.grid.getNode(node1.row, node1.col - 1).setAsEmptyNode();
-            }
-        } else {
-            if (node1.row - node2.row < 0) {
-                this.grid.getNode(node1.row + 1, node1.col).setAsEmptyNode();
-            } else {
-                this.grid.getNode(node1.row - 1, node1.col).setAsEmptyNode();
-            }
         }
     }
 }
