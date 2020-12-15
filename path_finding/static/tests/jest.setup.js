196d6e8733efdcc2de586941386ecedb004f92e2
Eric Dang <edang830@gmail.com>
2020-12-14 16:35:22 -0800

    Install boostrap-select

--- path_finding/static/tests/jest.setup.js
+++ path_finding/static/tests/jest.setup.js
@@ -1,1 +1,5 @@
 const domTests = require('@testing-library/jest-dom');
+global.window = window;
+global.$ = require('jquery');
+const bootstrapSelect = require('bootstrap-select');
+$.fn.selectpicker.Constructor.BootstrapVersion = '4';
