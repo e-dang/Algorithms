8fc14556c73125c10cd8892caf04df06f60917a0
Eric Dang <edang830@gmail.com>
2020-12-14 19:36:12 -0800

    Fix bug in prims algorithm

--- path_finding/static/src/maze_generators/prim.js
+++ path_finding/static/src/maze_generators/prim.js
@@ -4,18 +4,19 @@ const utils = require('../utils/utils');
 class RandomizedPrims extends BaseGenerator {
     async generate() {
         this.grid.nodes.forEach((node) => node.setAsWallNode(false));
-        const isWallNode = (node) => node.isWallNode();
+        const isWallNode = (node) => node.isWallNode() || node.isEndNode();
         const isPassageNode = (node) => !node.isWallNode();
 
         const walls = this.getNeighbors(this.grid.getStartNode(), isWallNode);
         while (walls.length) {
             const wall = utils.getRandom(walls);
-            if (wall.isWallNode()) {
+            if (isWallNode(wall)) {
                 const passages = this.getNeighbors(wall, isPassageNode);
-                const passage = utils.getRandom(passages);
-                await this.connect(passage, wall);
-
-                this.getNeighbors(wall, isWallNode).forEach((node) => walls.push(node));
+                if (passages.length !== 0) {
+                    const passage = utils.getRandom(passages);
+                    await this.connect(passage, wall);
+                    this.getNeighbors(wall, isWallNode).forEach((node) => walls.push(node));
+                }
             }
 
             walls.splice(walls.indexOf(wall), 1);
