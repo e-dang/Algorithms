c3336ed810ec0e25f30979afb41601e28c0f1787
Eric Dang <edang830@gmail.com>
2020-12-15 16:32:51 -0800

    Add bootstrap toggle to dev dependencies

--- path_finding/static/tests/jest.setup.js
+++ path_finding/static/tests/jest.setup.js
@@ -1,5 +1,7 @@
 const domTests = require('@testing-library/jest-dom');
 global.window = window;
 global.$ = require('jquery');
+global.jQuery = global.$;
 const bootstrapSelect = require('bootstrap-select');
 $.fn.selectpicker.Constructor.BootstrapVersion = '4';
+const bootstrapToggle = require('bootstrap-toggle');
