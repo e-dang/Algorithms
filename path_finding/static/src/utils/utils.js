9193c583dd17b89552be987d7910d2cc0a535141
Eric Dang <edang830@gmail.com>
2020-12-16 01:59:28 -0800

    Change order of directions in movements

--- path_finding/static/src/utils/utils.js
+++ path_finding/static/src/utils/utils.js
@@ -6,14 +6,23 @@ function getRandom(list) {
     return list[Math.floor(Math.random() * list.length)];
 }
 
+/*
+
+direction |  u  |  ul |  l  |  dl |  d  |  dr  |  r  |  ur |
+------------------------------------------------------------
+dr        | -1  | -1  |  0  |  1  |  1  |  1   |  0  |  -1 |
+------------------------------------------------------------
+dc        |  0  | -1  |  -1 | -1  |  0  |  1   |  1  |  1  |
+
+*/
 const manhattan_moves = {
     dr: [-1, 0, 1, 0],
     dc: [0, -1, 0, 1],
 };
 
 const diagonal_moves = {
-    dr: [-1, 0, 1, 0, -1, -1, 1, 1],
-    dc: [0, -1, 0, 1, -1, 1, -1, 1],
+    dr: [-1, -1, 0, 1, 1, 1, 0, -1],
+    dc: [0, -1, -1, -1, 0, 1, 1, 1],
 };
 
 module.exports = {
